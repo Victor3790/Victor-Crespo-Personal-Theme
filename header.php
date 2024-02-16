aed15b9a70a505e63919f7374da274c136b4c315
victor <victor3790@gmail.com>
2024-02-15 20:59:28 -0600

    Blog name added on title tag

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
+	<title> <?php echo esc_html( get_bloginfo( 'name' ) ); ?> </title>
 	<?php wp_head(); ?>
 </head>
 <body>
