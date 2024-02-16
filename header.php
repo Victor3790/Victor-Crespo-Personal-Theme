178948340f9d087589da4f33e25774a2f5f604dd
victor <victor3790@gmail.com>
2024-02-15 21:10:33 -0600

    Blog info added

--- header.php
+++ header.php
@@ -7,11 +7,11 @@
 
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 <head>
-	<meta charset="UTF-8">
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title> <?php echo esc_html( get_bloginfo( 'name' ) ); ?> </title>
+	<title> <?php bloginfo( 'name' ); ?> </title>
 	<?php wp_head(); ?>
 </head>
 <body>
